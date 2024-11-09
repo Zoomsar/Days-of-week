dd8ea9db77818fdbd83e01fc91f9f8dba4025e65
Rammus <rasmus.allmere@gmail.com>
2024-11-09 23:17:15 +0200

    Added every day yo

--- index.js
+++ index.js
@@ -17,20 +17,89 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req,res) => {
     const today = new Date();
     const day = today.getDay();
+    const currentYear = today.getFullYear();
     // console.log(day);
-    let type = "a weekday";
-    let adv = "its time to wrok blyat";
-    let a1 = "images/nuu_spungbob.jpg";
-    let musik = "audio/pink.mp3";
+    let type, adv, pic, musik;
 
-    if (day === 0 || day === 5 || day === 6) {
-        type = "the weeknd";
+    const musicMonday = [
+        "audio/pink.mp3", 
+        "audio/AIC.mp3",
+        "audio/ComfoNumb.mp3"
+    ];
+    const musicTuesday = [
+        "audio/IGOR.mp3",
+        "audio/MakeItWitChu.mp3",
+        "audio/Swimmin.mp3"
+    ];
+    const musicWednesday = [
+        "audio/Fascination.mp3",
+        "audio/WithYou.mp3",
+        "audio/RH.mp3"
+    ];
+    const musicThursday = [
+        "audio/NoMoreSorrow.mp3",
+        "audio/TakeMeToHo.mp3",
+        "audio/Rollin.mp3"
+    ];
+    const musicFriday = [
+        "audio/FridayCure.mp3",
+        "audio/Kaisarion.mp3",
+        "audio/volbeat.mp3"
+    ];
+    const musicSaturday = [
+        "audio/ADHD.mp3",
+        "audio/Getlucky.mp3",
+        "audio/Suit.mp3"
+    ];
+    const musicSunday = [
+        "audio/Arctic.mp3",
+        "audio/Doors.mp3",
+        "audio/Sunday_Blur.mp3"
+    ];
+
+    if (day === 1) {
+        type = "Rise and shine sailors, it be monday!";
+        adv = "Good luck!";
+        pic = "images/monda.png";
+        musik = musicMonday[Math.floor(Math.random() * musicMonday.length)];
+    } else if (day === 2) {
+        type = "Its chewsday init";
+        adv = "...";
+        pic = "images/chewsday.png";
+        musik = musicTuesday[Math.floor(Math.random() * musicTuesday.length)];
+    } else if (day === 3) {
+        type = "Wat do wensday be";
+        adv = "ughhhhh";
+        pic = "images/wensday.png";
+        musik = musicWednesday[Math.floor(Math.random() * musicWednesday.length)];
+    } else if (day === 4) {
+        type = "Its chursday!";
+        adv = "Ong almost there";
+        pic = "images/chursday.jpg";
+        musik = musicThursday[Math.floor(Math.random() * musicThursday.length)];
+    } else if (day === 5) {
+        type = "Iz friday";
         adv = "TIME TO PARTEYYY!!!!!!!";
-        a1 = "images/Spungobb_yeaah.jpg";
-        musik = "audio/volbeat.mp3";
+        pic = "images/Spungobb_yeaah.jpg";
+        musik = musicFriday[Math.floor(Math.random() * musicFriday.length)];
+    } else if (day === 6) {
+        type = "Happy Saturday!";
+        adv = "Time to grind!";
+        pic = "images/Spungobb_yeaah.jpg";
+        musik = musicSaturday[Math.floor(Math.random() * musicSaturday.length)];
+    } else if (day === 0) {
+        type = "It be sunday sunday";
+        adv = "Take it eze";
+        pic = "images/sunday.png";
+        musik = musicSunday[Math.floor(Math.random() * musicSunday.length)];
     }
 
-    res.render("index.ejs", {dayType: type, advice: adv, a1, musik});
+    res.render("index.ejs", {
+        dayType: type,
+        advice: adv,
+        pic,
+        musik,
+        year: currentYear});
 });
 
 app.listen(port, () => {
